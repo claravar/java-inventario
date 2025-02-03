abbd641250e449d90e1c71635e03867073e2d899
clara.varela <clara.varela@live.com>
2025-02-03 16:11:14 -0600

    Implementacion editar y eliminar producto

--- src/main/java/gm/inventarios/Modelo/Producto.java
+++ src/main/java/gm/inventarios/Modelo/Producto.java
@@ -3,10 +3,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 @Entity
 @Data
@@ -16,8 +13,45 @@ import lombok.ToString;
 public class Producto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public Integer idProducto;
-    public String descripcion;
-    public Double precio;
-    public Integer existencia;
+    private Integer idProducto;
+    private String descripcion;
+    private Double precio;
+    private Integer existencia;
+
+    // Getter y Setter para idProducto
+    public Integer getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Integer idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    // Getter y Setter para descripcion
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    // Getter y Setter para precio
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    // Getter y Setter para existencia
+    public Integer getExistencia() {
+        return existencia;
+    }
+
+    public void setExistencia(Integer existencia) {
+        this.existencia = existencia;
+    }
+
 }
